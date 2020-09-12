bf760597489fd87ffb65758f0a19b1044b7862a7
Zac Smith <zrs3141592@gmail.com>
2020-09-12 16:27:55 -0600

    create template changes

---
+++ templates/App.js
@@ -0,0 +1,20 @@
+module.exports = `import React from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome Lambda student! This is filler, not need to keep it!</h2>
+      <a href="https://lambdaschool.com/">Visit the Lambda Llama home</a>
+      <br />
+      <img
+        alt="Lambda loves our llamas!"
+        style={{ width: '300px', height: 'auto' }}
+        src="https://images.pexels.com/photos/1578484/pexels-photo-1578484.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+      ></img>
+    </div>
+  )
+}
+
+export default App`
---
+++ templates/index.js
@@ -0,0 +1,11 @@
+module.exports = `import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)`

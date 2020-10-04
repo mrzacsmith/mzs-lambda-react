7376ca0c2e6c09bf015c31562e1421bb079cb6bf
Michael Phelps <michaeljphelps1978@gmail.com>
2020-10-04 00:30:52 -0500

    :lipstick: App.css Template
    creates App.css template file for issue #3

---
+++ templates/App.css.js
@@ -0,0 +1,53 @@
+module exports = `@import url("https://fonts.googleapis.com/css2?family=Crimson+Text:wght@600&display=swap");
+
+body {
+  background-color: #1d212c;
+}
+
+h2 {
+  color: #ec3944;
+  font-family: "Crimson Text", serif;
+  text-align: center;
+  font-size: 3.5rem;
+}
+
+.image-container {
+  display: flex;
+  justify-content: center;
+  padding-top: 40px;
+}
+
+img {
+  width: 30%;
+  text-align: center;
+}
+
+h2 {
+  color: white;
+}
+
+.welcome-bottom {
+  margin-top: -10px;
+  color: #ec3944;
+  font-size: 3rem;
+}
+
+h4 {
+  text-align: center;
+  color: #f9eee5;
+}
+
+.filler-text {
+  text-align: center;
+  color: white;
+  padding-top: 5%;
+}
+
+.footer {
+  text-align: center;
+  padding-top: 10%;
+}
+
+.footer p {
+  color: white;
+}`
